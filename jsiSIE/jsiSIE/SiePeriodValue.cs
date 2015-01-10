d65c191ec2ac02fb99e721501e8f0ca68f71cb7a
Johan Idsam <johan@jsisoft.se>
2015-01-10 12:46:15 +0100

    Added ToVoucherRow and ToInvertedVourhcerRow to SiePeriodBalance. Helps when creating vouchers for VAT, and Closing Books.

--- jsiSIE/jsiSIE/SiePeriodValue.cs
+++ jsiSIE/jsiSIE/SiePeriodValue.cs
@@ -15,5 +15,26 @@ namespace jsiSIE
         public decimal? Quantity;
         public List<SieObject> Objects;
         public string Token;
+
+        public SieVoucherRow ToVoucherRow()
+        {
+            var vr = new SieVoucherRow()
+            {
+             Account = this.Account,
+             Amount = this.Amount,
+             Objects = this.Objects,
+             Quantity = this.Quantity
+            }
+            return vr;
+        }
+
+        public SieVoucherRow ToInvertedVoucherRow()
+        {
+            var vr = ToVoucherRow();
+            vr.Amount *= -1;
+
+            return vr;
+        }
+
     }
 }
