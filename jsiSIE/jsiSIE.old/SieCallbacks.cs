c0b66c88eb141ba4ff147f576e4267894b5b2e73
johan idstam <johan@idstam.com>
2020-11-19 15:25:44 +0100

    Changed lib to net standard 2.0 and uplaoded new nuget package.

---
+++ jsiSIE/jsiSIE.old/SieCallbacks.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsiSIE
+{
+    public class SieCallbacks
+    {
+        public Action<string> Line;
+        public void CallbackLine(string message)
+        {
+            if(Line != null) Line(message);
+        }
+
+        public Action<Exception> SieException;
+        public void CallbackException(Exception ex)
+        {
+            if(SieException != null) SieException(ex);
+        }
+
+        public Action<SiePeriodValue> IB;
+        internal void CallbackIB(SiePeriodValue pv)
+        {
+            if (IB != null) IB(pv);
+        }
+
+        public Action<SiePeriodValue> UB;
+        internal void CallbackUB(SiePeriodValue pv)
+        {
+            if (UB != null) UB(pv);
+        }
+
+        public Action<SiePeriodValue> OIB;
+        internal void CallbackOIB(SiePeriodValue pv)
+        {
+            if (OIB != null) OIB(pv);
+        }
+
+        public Action<SiePeriodValue> OUB;
+        internal void CallbackOUB(SiePeriodValue pv)
+        {
+            if (OUB != null) OUB(pv);
+        }
+
+        public Action<SiePeriodValue> PSALDO;
+        internal void CallbackPSALDO(SiePeriodValue pv)
+        {
+            if (PSALDO != null) PSALDO(pv);
+        }
+
+        public Action<SiePeriodValue> PBUDGET;
+        internal void CallbackPBUDGET(SiePeriodValue pv)
+        {
+            if (PBUDGET != null) PBUDGET(pv);
+        }
+
+        public Action<SiePeriodValue> RES;
+        internal void CallbackRES(SiePeriodValue pv)
+        {
+            if (RES != null) RES(pv);
+        }
+
+        public Action<SieVoucher> VER;
+        internal void CallbackVER(SieVoucher v)
+        {
+            if (VER != null) VER(v);
+        }
+    }
+}
---
+++ jsiSIE/jsiSIE.old/SieDimension.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jsiSIE
+{
+    [Serializable]
+    public class SieDimension
+    {
+        public string Number { get; set; }
+        public string Name { get; set; }
+        public bool IsDefault = false;
+
+        private SieDimension _parent = null;
+        public SieDimension SuperDim 
+        { 
+            get
+            {
+                return _parent;
+            }
+            set
+            {
+                _parent = value;
+                _parent.SubDim.Add(this);
+            } 
+        }
+        public HashSet<SieDimension> SubDim = new HashSet<SieDimension>();
+        public Dictionary<string, SieObject> Objects = new Dictionary<string, SieObject>();
+        
+    }
+}
