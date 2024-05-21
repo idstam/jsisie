4e52d9877c75584c20fecf221f257f687b5bceca
johan idstam <johan@idstam.com>
2024-05-21 12:19:14 +0200

    Added more CodePages so that 437 should work.

--- jsiSIE/jsiSIE/EncodingHelper.cs
+++ jsiSIE/jsiSIE/EncodingHelper.cs
@@ -6,18 +6,16 @@ namespace jsiSIE
     public class EncodingHelper
     {
         
-        private static int _default = 0;
+        private static int _default = 28591; //ISO-8859-1 (single byte fallback)
         public static Encoding GetDefault(){
             if(_default == 0)
             {
-                //Dotnet core does not have codepage 437 that is the standard for SIE
-                //I fall back to ISO-8859-1 as that is a single byte code page so that KSUMMA can be calculated.
+                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
                 var encodings = Encoding.GetEncodings();
+
                 if(encodings.Any(e => e.CodePage == 437)){
-                    _default = 437;
-                } else {
-                    _default = 28591;
-                }
+                    _default = 437; //PC-8 CP437 (SIE standard)
+                } 
             }
 
             return Encoding.GetEncoding(_default);
