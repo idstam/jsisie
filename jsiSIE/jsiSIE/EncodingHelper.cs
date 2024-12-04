fc3febf9573cb301bdc2a059a850e1e41eb24b16
Johan Idstam <johan@idstam.com>
2024-12-04 18:02:34 +0100

    Fixed bug in encoding helper.

--- jsiSIE/jsiSIE/EncodingHelper.cs
+++ jsiSIE/jsiSIE/EncodingHelper.cs
@@ -6,7 +6,7 @@ namespace jsiSIE
     public class EncodingHelper
     {
         
-        private static int _default = 28591; //ISO-8859-1 (single byte fallback)
+        private static int _default = 0; 
         public static Encoding GetDefault(){
             if(_default == 0)
             {
@@ -15,7 +15,11 @@ namespace jsiSIE
 
                 if(encodings.Any(e => e.CodePage == 437)){
                     _default = 437; //PC-8 CP437 (SIE standard)
-                } 
+                }
+                else
+                {
+                    _default = 28591; //ISO-8859-1 (single byte fallback)
+                }
             }
 
             return Encoding.GetEncoding(_default);
